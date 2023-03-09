0e22c5457f51cd5083d85c50fa3e44bbead23c50
unknown <anamurrahmanlaraib@gmail.com>
2023-03-09 17:59:57 +0530

    try 1

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package com.driver.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.driver.services.AdminService;
@@ -24,34 +25,47 @@ public class AdminServiceImpl implements AdminService {
 
 	@Autowired
 	CustomerRepository customerRepository1;
+	private List<Driver> listOfDrivers = new ArrayList<>();
+	private List<Customer> listOfCustomers = new ArrayList<>();
 
 	@Override
 	public void adminRegister(Admin admin) {
 		//Save the admin in the database
+		adminRepository1.save(admin);
 	}
 
 	@Override
 	public Admin updatePassword(Integer adminId, String password) {
 		//Update the password of admin with given id
+		Admin admin = adminRepository1.findById(adminId).get();
+
+		admin.setPassword(password);
+
+		adminRepository1.save(admin);
+
+		return admin;
 
 	}
 
 	@Override
 	public void deleteAdmin(int adminId){
 		// Delete admin without using deleteById function
+		Admin admin = adminRepository1.findById(adminId).get();
+		adminRepository1.delete(admin);
 
 	}
 
 	@Override
 	public List<Driver> getListOfDrivers() {
 		//Find the list of all drivers
-
+		return driverRepository1.findAll();
 	}
 
 	@Override
 	public List<Customer> getListOfCustomers() {
 		//Find the list of all customers
-
+		listOfCustomers = customerRepository1.findAll();
+		return listOfCustomers;
 	}
 
 }
